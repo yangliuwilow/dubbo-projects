6668ae1f76e429bbcc643b7887182863183f937d
yangliu <342585705@qq.com>
2018-07-16 11:33:15 +0800

    add jsp

--- spring-dubbo-mybatis/spring-dubbo-mybatis-consumer/src/main/java/com/willow/SpringDubboConsumer.java
+++ spring-dubbo-mybatis/spring-dubbo-mybatis-consumer/src/main/java/com/willow/SpringDubboConsumer.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class SpringDubboConsumer {
 
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"spring-consumer.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"spring-consumer.xml","spring-mvc.xml"});
         context.start();
        while(true){
            Thread.sleep(1000L);
